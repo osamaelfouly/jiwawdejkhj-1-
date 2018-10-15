293a0ff874a81066db15047756212fd05ed9fabf
osamaelfouly <42895946+osamaelfouly@users.noreply.github.com>
2018-10-15 15:41:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const jimp = require('jimp');
 const Canvas = require('canvas');
 
 client.on('guildMemberAdd', member => {
