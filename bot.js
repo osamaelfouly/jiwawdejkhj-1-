99e745473165d10d5704028e83574df55cbc1459
osamaelfouly <42895946+osamaelfouly@users.noreply.github.com>
2018-10-15 19:44:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const Canvas = require('canvas');
-
+const Canvas = require('canvas-prebuilt');
 client.on('guildMemberAdd', member => {
      const welcomer =  member.guild.channels.find('name', 'welcome');
     if(!welcomer) return;
